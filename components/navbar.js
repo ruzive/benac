c193583c8d95cb9767aee97663f964a0d7ba1474
ruzive <ruzive@gmail.com>
2024-05-14 11:14:50 +0100

    image play

--- components/navbar.js
+++ components/navbar.js
@@ -22,7 +22,7 @@ const Navbar = () => {
                   <span className="flex items-center space-x-2 text-2xl font-medium text-indigo-500 dark:text-gray-100">
                     <span>
                       <Image
-                        src="/img/logo.svg"
+                        src="/favicon.ico"
                         alt="N"
                         width="32"
                         height="32"
